b2a9f35d82f42f82f18186d882119df36a4afc4a
Tobias Bernard <tobias.bernard@futurice.com>
2017-05-11 13:21:35 +0200

    [fix] carousel size issue due to js executing before images are loaded

--- script.js
+++ script.js
@@ -1,33 +1,35 @@
-var flkty = new Flickity('.carousel');
+window.onload = function() {
+  var flkty = new Flickity('.carousel');
 
-$( "#menubutton" ).click(function() {
-  $( "#nav-popover" ).toggleClass( "collapsed" );
-  $( "#nav" ).toggleClass( "inactive" );
-});
+  $( "#menubutton" ).click(function() {
+    $( "#nav-popover" ).toggleClass( "collapsed" );
+    $( "#nav" ).toggleClass( "inactive" );
+  });
 
-$( "#preview-tab" ).click(function() {
-  flkty.select(0);
-  $( ".tab" ).removeClass( "active" );
-  $( this ).addClass( "active" );
-});
-$( "#journey-tab" ).click(function() {
-  flkty.select(1);
-  $( ".tab" ).removeClass( "active" );
-  $( this ).addClass( "active" );
-});
-$( "#data-tab" ).click(function() {
-  flkty.select(2);
-  $( ".tab" ).removeClass( "active" );
-  $( this ).addClass( "active" );
-});
+  $( "#preview-tab" ).click(function() {
+    flkty.select(0);
+    $( ".tab" ).removeClass( "active" );
+    $( this ).addClass( "active" );
+  });
+  $( "#journey-tab" ).click(function() {
+    flkty.select(1);
+    $( ".tab" ).removeClass( "active" );
+    $( this ).addClass( "active" );
+  });
+  $( "#data-tab" ).click(function() {
+    flkty.select(2);
+    $( ".tab" ).removeClass( "active" );
+    $( this ).addClass( "active" );
+  });
 
-flkty.on( 'select', function() {
-  $( ".tab" ).removeClass( "active" );
-  switch (flkty.selectedIndex) {
-    case 0: $( "#preview-tab" ).addClass( "active" );
-    break;
-    case 1: $( "#journey-tab" ).addClass( "active" );
-    break;
-    case 2: $( "#data-tab" ).addClass( "active" );
-  }
-});
+  flkty.on( 'select', function() {
+    $( ".tab" ).removeClass( "active" );
+    switch (flkty.selectedIndex) {
+      case 0: $( "#preview-tab" ).addClass( "active" );
+      break;
+      case 1: $( "#journey-tab" ).addClass( "active" );
+      break;
+      case 2: $( "#data-tab" ).addClass( "active" );
+    }
+  });
+}
